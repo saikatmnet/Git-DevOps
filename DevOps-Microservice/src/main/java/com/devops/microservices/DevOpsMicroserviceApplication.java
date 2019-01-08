95f6dd8481dec79ecda35323949dc8d8b9559b51
Saikat Majumder <smajumder@sapient.com>
2019-01-08 17:39:59 +0530

    New Module

---
+++ DevOps-Microservice/src/main/java/com/devops/microservices/DevOpsMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.devops.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsMicroserviceApplication.class, args);
+	}
+
+}
+
